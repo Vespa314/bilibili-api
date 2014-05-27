a72672adfae7cc4d6e443ac325d3ce98e6a49dff
Vespa <kylensherlock@gmail.com>
2014-05-28 01:45:24 +0800

    增加获取用户信息和获取专题视频API

--- python API/bilibili.py
+++ python API/bilibili.py
@@ -8,7 +8,7 @@ Created on Mon May 26 23:42:03 2014
 import sys, time, os, re
 from support import * 
 
-#常量定义
+############################常量定义
 
 #####排序方式
 #收藏
@@ -27,88 +27,7 @@ TYPE_DANMU = 'damku'
 TYPE_PINYIN = 'pinyin'
 #投稿时间
 TYPE_TOUGAO = 'default'
-
-#常量定义结束
-
-
-class User():
-    def __init__(self):
-        pass
-    def __init__(self,m_mid,m_name):
-        self.mid = m_mid;
-        self.name = m_name;
-    def saveToFile(self,fid):
-        fid.write('\t名字:%s\n'%self.name);
-        fid.write('\tid:%s\n'%self.mid);
-#   获取空间地址
-    def GetSpace(self):
-        return 'http://space.bilibili.tv/'+str(mid);
-    mid = None;
-    name = None;
-
-class Vedio():
-    def __init__(self):
-        pass
-#   写到文件中
-    def saveToFile(self,fid):
-        fid.write('av号:%d\n'%self.aid);
-        fid.write('标题:%s\n'%self.title);
-        fid.write('观看:%d\n'%self.guankan);
-        fid.write('收藏:%d\n'%self.shoucang);
-        fid.write('弹幕:%d\n'%self.danmu);
-        fid.write('日期:%s\n'%self.date);
-        fid.write('封面地址:%s\n'%self.cover);
-        fid.write('Up主:\n');
-        self.po.saveToFile(fid);
-        fid.write('\n');
-    aid = None;
-    title = None;
-    guankan = None;
-    shoucang = None;
-    danmu = None;
-    date = None;
-    cover = None;
-    po = None;
-#从视频源码获取视频信息
-def GetVedioFromRate(content):
-    #av号和标题
-    regular1 = r'<a href="/video/av(\d+)/" target="_blank" class="title">([^/]+)</a>';
-    info1 = GetRE(content,regular1)
-    #观看数
-    regular2 = r'<i class="gk" title=".*">(\d+)</i>';
-    info2 = GetRE(content,regular2)
-    #收藏
-    regular3 = r'<i class="sc" title=".*">(\d+)</i>';
-    info3 = GetRE(content,regular3)
-    #弹幕
-    regular4 = r'<i class="dm" title=".*">(\d+)</i>';
-    info4 = GetRE(content,regular4)
-    #日期
-    regular5 = r'<i class="date" title=".*">(\d+-\d+-\d+ \d+:\d+)</i>';
-    info5 = GetRE(content,regular5)
-    #封面
-    regular6 = r'<img src="(.+)">';
-    info6 = GetRE(content,regular6)
-    #Up的id和名字
-    regular7 = r'<a class="up r10000" href="http://space.bilibili.tv/(\d+)" target="_blank">(.+)</a>'
-    info7 = GetRE(content,regular7)
-    #!!!!!!!!这里可以断言所有信息长度相等
-    vedioNum = len(info1);#视频长度
-    vedioList = [];
-    for i in range(vedioNum):
-        vedio_t = Vedio();
-        vedio_t.aid = int(info1[i][0]);
-        vedio_t.title = info1[i][1];
-        vedio_t.guankan = int(info2[i]);
-        vedio_t.shoucang = int(info3[i]);
-        vedio_t.danmu = int(info4[i]);
-        vedio_t.date = info5[i];
-        vedio_t.cover = info6[i];
-        vedio_t.po = User(info7[i][0],info7[i][1])
-        vedioList.append(vedio_t);
-    return vedioList
-
-
+############################常量定义结束
 
 def GetPopularVedio(begintime,endtime,sortType=TYPE_BOFANG,zone=0,page=1,original=0):
     """
@@ -135,12 +54,93 @@ def GetPopularVedio(begintime,endtime,sortType=TYPE_BOFANG,zone=0,page=1,origina
     content = getURLContent(url);
     return GetVedioFromRate(content);
 
+def GetUserInfo(url):
+    """
+由GetUserInfoBymid(mid)或者GetUserInfoByName(name)调用
+返回：
+    用户信息
+待添加：
+    如果用户不存在返回的是：{"code":-626,"message":"User is not exists."}
+    """
+    content = getURLContent(url)
+    jsoninfo = FromJson(content);
+    user = User(jsoninfo['mid'],jsoninfo['name'].encode('utf8'));
+    user.isApprove = jsoninfo['approve'];
+    user.spaceName = jsoninfo['spacename'].encode('utf8');
+    user.sex = jsoninfo['sex'].encode('utf8');
+    user.rank = jsoninfo['rank'];
+    user.avatar = jsoninfo['face'];
+    user.follow = jsoninfo['attention'];
+    user.fans = jsoninfo['fans'];
+    user.article = jsoninfo['article'];
+    user.place = jsoninfo['place'];
+    user.description = jsoninfo['description'];
+    user.followlist = [];
+    for fo in jsoninfo['attentions']:
+        user.followlist.append(jsoninfo['attentions'][fo])
+    return user;
 
+def GetUserInfoBymid(mid):
+    """
+输入：
+    mid：查询的用户的id
+返回：
+    查看GetUserInfo()函数
+    """
+    mid = GetString(mid);
+    url = 'http://api.bilibili.cn/userinfo'+"?mid="+mid;
+    return GetUserInfo(url)
+    
+def GetUserInfoByName(name):
+    """
+输入：
+    mid：查询的用户的昵称
+返回：
+    查看GetUserInfo()函数
+    """
+    name = GetString(name);
+    url = 'http://api.bilibili.cn/userinfo'+"?user="+name;
+    print url
+    return GetUserInfo(url)
 
+def GetVedioOfZhuanti(spid,season_id=None,bangumi=None):
+"""
+输入：
+    spid:专题id
+    season_id：分季ID
+    bangumi：设置为1返回剧番，不设置或者设置为0返回相关视频
+返回：
+    视频列表，包含av号，标题，封面和观看数
+"""
+    url = ' http://api.bilibili.cn/spview?spid='+GetString(spid);
+    if season_id != None:
+        url += '&season_id='+GetString(season_id);
+    if bangumi != None:
+        url += '&bangumi='+GetString(bangumi);
+    content = getURLContent(url)
+    jsoninfo = FromJson(content);
+    vediolist = [];
+    for vedio_idx in jsoninfo['list']:
+        vedio = Vedio(jsoninfo['list'][vedio_idx]['aid'],jsoninfo['list'][vedio_idx]['title']);
+        vedio.cover = jsoninfo['list'][vedio_idx]['cover'];
+        vedio.guankan = jsoninfo['list'][vedio_idx]['click'];
+        vediolist.append(vedio);
+    return vediolist
 
 if __name__ == "__main__":
-    vedioList = GetPopularVedio([2014,05,20],[2014,05,27],TYPE_BOFANG,0,1)
-    f = open('result.txt','w');
-    for vedio in vedioList:
-        vedio.saveToFile(f);
-    f.close();
+#     f = open('result.txt','w');
+     
+#    vedioList = GetPopularVedio([2014,05,20],[2014,05,27],TYPE_BOFANG,0,1)
+#    for vedio in vedioList:
+#        vedio.saveToFile(f);
+     
+#     user = GetUserInfoBymid('72960');
+#     print user.name.decode('utf8','ignore').encode('gbk','ignore')
+#     user = GetUserInfoByName('vespa')
+#     print user.spaceName.decode('utf8','ignore').encode('gbk','ignore')
+##    user.saveToFile(f);    
+    
+#    vediolist = GetVedioOfZhuanti('6492',bangumi=0);
+#    for vedio in vediolist:
+#        print vedio.title
+#     f.close();
