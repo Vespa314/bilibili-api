f994ff09abed49a7ebc4c58d14f16989f8aed2e7
Vespa <kylensherlock@gmail.com>
2015-02-21 11:07:37 +0800

    移除非api函数到support.py

--- python API/bilibili.py
+++ python API/bilibili.py
@@ -54,46 +54,6 @@ def GetPopularVedio(begintime, endtime, sortType=TYPE_BOFANG, zone=0, page=1, or
     content = getURLContent(url)
     return GetVedioFromRate(content)
 
-def GetVedioFromRate(content):
-    """
-从视频搜索源码页面提取视频信息
-    """
-    #av号和标题
-    regular1 = r'<a href="/video/av(\d+)/" target="_blank" class="title">([^/]+)</a>'
-    info1 = GetRE(content,regular1)
-    #观看数
-    regular2 = r'<i class="gk" title=".*">(.+)</i>'
-    info2 = GetRE(content,regular2)
-    #收藏
-    regular3 = r'<i class="sc" title=".*">(.+)</i>'
-    info3 = GetRE(content,regular3)
-    #弹幕
-    regular4 = r'<i class="dm" title=".*">(.+)</i>'
-    info4 = GetRE(content,regular4)
-    #日期
-    regular5 = r'<i class="date" title=".*">(\d+-\d+-\d+ \d+:\d+)</i>'
-    info5 = GetRE(content,regular5)
-    #封面
-    regular6 = r'<img src="(.+)">'
-    info6 = GetRE(content,regular6)
-    #Up的id和名字
-    regular7 = r'<a class="up r10000" href="http://space\.bilibili\.com/(\d+)" target="_blank">(.+)</a>'
-    info7 = GetRE(content,regular7)
-    #!!!!!!!!这里可以断言所有信息长度相等
-    vedioNum = len(info1)#视频长度
-    vedioList = []
-    for i in range(vedioNum):
-        vedio_t = Vedio()
-        vedio_t.aid = getint(info1[i][0])
-        vedio_t.title = info1[i][1]
-        vedio_t.guankan = getint(info2[i])
-        vedio_t.shoucang = getint(info3[i])
-        vedio_t.danmu = getint(info4[i])
-        vedio_t.date = info5[i]
-        vedio_t.cover = info6[i]
-        vedio_t.author = User(info7[i][0],info7[i][1])
-        vedioList.append(vedio_t)
-    return vedioList
 
 def GetUserInfo(url):
     """
@@ -263,25 +223,6 @@ def GetVedioInfo(aid, appkey,page = 1, AppSecret=None, fav = None):
     vedio.instant_server = jsoninfo.Getvalue('instant_server')
     return vedio
 
-def GetSign(params, appkey, AppSecret=None):
-    """
-    获取新版API的签名，不然会返回-3错误
-待添加：【重要！】
-    需要做URL编码并保证字母都是大写，如 %2F
-    """
-    params['appkey']=appkey
-    data = ""
-    paras = params.keys()
-    paras.sort()
-    for para in paras:
-        if data != "":
-            data += "&"
-        data += para + "=" + str(params[para])
-    if AppSecret == None:
-        return data
-    m = hashlib.md5()
-    m.update(data+AppSecret)
-    return data+'&sign='+m.hexdigest()
 
 def GetGangumi(appkey, btype = None, weekday = None, AppSecret=None):
     """
